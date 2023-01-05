76cf6742c54bfb56b7f7452544fe1914b879bffa
Aleksey Savchenko <wasxela@gmail.com>
2023-01-05 16:28:30 +0200

    remade connection to db throw env.local

--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,8 @@
 import { Collection, MongoClient, ObjectId, UpdateResult } from 'mongodb';
 import { IPlayerInfo } from '../types/globalTypes';
+import process from 'node:process';
 
-const URL = 'mongodb+srv://user_1:user_1@learningcluster.3ozfb.mongodb.net/?retryWrites=true&w=majority';
+// const URL = 'mongodb+srv://user_1:user_1@learningcluster.3ozfb.mongodb.net/?retryWrites=true&w=majority';
 
 interface IGunnersSchema {
   _id: ObjectId | string;
@@ -9,7 +10,7 @@ interface IGunnersSchema {
   data: IPlayerInfo[];
 }
 
-const client = new MongoClient(URL);
+const client = new MongoClient(process.env.DOMAIN_DB!);
 
 const connectDb = (): Collection<IGunnersSchema> => {
   const database = client.db('western_shot');
